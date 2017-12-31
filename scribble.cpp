c419e48101060a261e9a1fb8eb56326200931afb
parth-shel <ps@parthshel.com>
2017-12-31 14:12:36 -0500

    made some changes

--- scribble.cpp
+++ scribble.cpp
@@ -161,7 +161,7 @@ int gd,gm,errorcode;
 //initialize graphics mode
 gd=DETECT;
 //initgraph(&gd,&gm,"");
-initwindow(635, 445);
+initwindow(640, 480);
 
 errorcode = graphresult();
 if (errorcode != grOk)  // an error occurred
