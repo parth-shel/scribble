f8d18ad291e31564d6dcd1079554273cbbcb0577
parth-shel <ps@parthshel.com>
2018-01-04 17:47:50 -0500

    made some changes

--- scribble.cpp
+++ scribble.cpp
@@ -983,8 +983,8 @@ else if(fillCircleToolBtn.press()){//filled circle tool btn
  	outtextxy(3,2,"Working on it...");
  	//fp.open(fname,ios::out);
  	fp = fopen(fname, "w+");
- 	for(sx=1;sx<=100;sx++) {
- 		for(sy=15;sy<=100;sy++) {
+ 	for(sx=1;sx<=639;sx++) {
+ 		for(sy=15;sy<=450;sy++) {
 			if(getpixel(sx,sy) != BLACK) {
 				sf.col=sx; sf.row=sy; sf.color=getpixel(sx,sy);
 				//fp<<sf.col<<','<<sf.row<<','<<sf.color<<',';
