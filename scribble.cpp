7dcea148c56ae331cca0867c2cbd606cf444dfd6
parth-shel <ps@parthshel.com>
2018-01-05 02:12:33 -0500

    made some changes

--- scribble.cpp
+++ scribble.cpp
@@ -530,7 +530,7 @@ while(1)
 			//showmouseptr();
 			//getmousepos(&button,&x,&y);
 			//if(x>1 && x<639 && y>15 && y<450 && button==1){
-				putpixel(x,y,pencolor);
+				//putpixel(x,y,pencolor);
 				x1=x;y1=y;
 				//break;
 				//}
@@ -576,7 +576,7 @@ else if(x>11+pensize && x<629-pensize && y<430-pensize && y>26+pensize && button
 		//showmouseptr();
 		//getmousepos(&button,&x,&y);
 		//if(x>1 && x<639 && y>15 && y<450 && button==1){
-		  putpixel(x,y,pencolor);
+		  //putpixel(x,y,pencolor);
 		  x1=x;y1=y;
 		  //break;
 		  //}
